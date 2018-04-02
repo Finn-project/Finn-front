07cd102b34faa3aeb0ea121278ad487df61e73cc
Ryanden <lockstom@gmail.com>
2018-04-02 15:22:55 +0900

    delete

--- src/app/finn/feature/host-register/host-register.module.ts
+++ src/app/finn/feature/host-register/host-register.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HostRegisterComponent } from './host-register.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HostRegisterComponent]
+  declarations: []
 })
 export class HostRegisterModule { }
