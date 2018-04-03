3b7dc055347a02923fddb7b295edd55d47183d80
choihjin <chlgurwls137@gmail.com>
2018-04-03 16:45:14 +0900

    edit core -component file

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CoreComponent } from './core.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './login/sign-up/sign-up.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CoreComponent, HeaderComponent, LoginComponent],
-  exports: [CoreComponent, HeaderComponent, LoginComponent]
+  declarations: [HeaderComponent, LoginComponent, SignUpComponent],
+  exports: [HeaderComponent, LoginComponent, SignUpComponent]
 })
 export class CoreModule { }
