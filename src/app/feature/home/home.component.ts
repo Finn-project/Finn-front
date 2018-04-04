7a06c31e4940910eaa5fbb286ef6c7a627cbe84c
chamchii <tw119kim@gmail.com>
2018-04-04 22:26:19 +0900

    room list layout

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -6,7 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  data = [{
+    title: 'hi hello',
+    roomType: '개인실',
+    price: '65000',
+    image: 'https://picsum.photos/500/984',
+    location: '서울'
+  }, {
+    title: 'hi hello',
+    roomType: '개인실',
+    price: '65000',
+    image: 'https://picsum.photos/1000/400',
+    location: '서울'
+  }, {
+    title: 'hi hello',
+    roomType: '개인실',
+    price: '65000',
+    image: 'https://picsum.photos/1200/984',
+    location: '서울'
+  }, {
+    title: 'hi hello',
+    roomType: '개인실',
+    price: '65000',
+    image: 'https://picsum.photos/1200/984',
+    location: '서울'
+  }, {
+    title: 'hi hello',
+    roomType: '개인실',
+    price: '65000',
+    image: 'https://picsum.photos/1200/984',
+    location: '서울'
+  }, {
+    title: 'hi hello',
+    roomType: '개인실',
+    price: '65000',
+    image: 'https://picsum.photos/1200/984',
+    location: '서울'
+  }, {
+    title: 'hi hello',
+    roomType: '개인실',
+    price: '65000',
+    image: 'https://picsum.photos/1200/984',
+    location: '서울'
+  }, {
+    title: 'hi hello',
+    roomType: '개인실',
+    price: '65000',
+    image: 'https://picsum.photos/1200/984',
+    location: '서울'
+  }]
   constructor() { }
 
   ngOnInit() {
