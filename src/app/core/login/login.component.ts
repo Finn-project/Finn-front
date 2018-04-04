844e4ca8e1d64a160069817f1c601b4d6954faf8
choihjin <chlgurwls137@gmail.com>
2018-04-04 20:37:37 +0900

    add form 유효성 검사

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,11 +10,104 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class LoginComponent implements OnInit {
   @Input() modal;
   @Output() loginModal = new EventEmitter();
-  constructor() { }
+  sigin: boolean;
+  url = 'https://himanmen.com/';
+
+  userForm: FormGroup;
+
+  signForm: FormGroup;
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
 
+    this.userForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/)
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/[a-zA-Z0-9]/),
+        Validators.minLength(4),
+        Validators.maxLength(10)
+      ]),
+    });
+
+    this.signForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/)
+      ]),
+      frist_name: new FormControl('',[
+        Validators.required
+      ]),
+      last_name: new FormControl('', [
+        Validators.required
+      ]),
+      password: new FormControl('',[
+        Validators.required
+      ]),
+      confirm_password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      phone_num: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\d{3}-\d{3,4}-\d{4}$/),
+      ]),
+    });
+  }
+  viewStatus() {
+    this.sigin = !this.sigin;
+    console.log(this.signForm.value.email);
+
   }
 
+
+
+  // 회원가입 form
+
+  get email_f() {
+    return this.signForm.get('email');
+  }
+  get frist_name() {
+    return this.signForm.get('frist_name');
+  }
+  get last_name() {
+    return this.signForm.get('last_name');
+
+  }
+  get password_f() {
+    return this.signForm.get('password');
+
+  }
+  get confirm_password() {
+    return this.signForm.get('confirm_password');
+
+  }
+  get phone_num() {
+    return this.signForm.get('phone_num');
+
+  }
+  // login form
+  get email() {
+    return this.userForm.get('email');
+  }
+  get password() {
+    return this.userForm.get('password');
+  }
+  sign() {
+
+    console.log(this.signForm);
+  }
+
+  login() {
+    console.log(this.userForm);
+  }
+  match () {
+    if (this.signForm.value.password !== this.signForm.value.confirm_password){
+      return true;
+    }
+  }
 
 }
